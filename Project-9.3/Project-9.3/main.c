2cf97dc902dc8aeedb4831025d862054ebc82510
GregKM <gregkmathews@gmail.com>
2014-11-04 13:08:47 -0500

    Completed Project 9.3 and commented code

--- Project-9.3/Project-9.3/main.c
+++ Project-9.3/Project-9.3/main.c
@@ -6,22 +6,35 @@
 
 #include <stdio.h> 
 
-void chLineRow(char ch, int c, int r);
+// Declare our function prototype to print character in columns and rown
+void printfunc(char ch, int c, int r);
 int main(void)
 {
+	// Declare vars for character and number of columns and rows
 	char ch;
-	int col, row;
+	int column;
+	int row;
 
+	// Ask user for input
 	printf("Enter a character (# to quit): ");
 	
+	// Control loop to check that quit symbol has not been entered
 	while ((ch = getchar()) != '#')
 	{
+		// Ignore new line
 		if (ch == '\n')
 			continue;
-		printf("Enter number of columns and number of rows: ");
-		if (scanf("%d %d", &col, &row) != 2)
+		// Ask user for columns
+		printf("Number of columns? ");
+		// Use scanf to get input from user, validate, and store in column variable
+		if (scanf("%d", &column) != 1)
 			break;
-		chLineRow(ch, col, row);
+		// Use scanf to get input from user, valdate, and store in row variable
+		printf("Number of rows? ");
+		if (scanf("%d", &row) != 1)
+			break;
+		// Call our print function and pass the values stored for character, columns, and rows
+		printfunc(ch, column, row);
 		printf("\nEnter next character (# to quit): ");
 	}
 	
@@ -30,16 +43,22 @@ int main(void)
 	return 0;
 }
 
-void chLineRow(char ch, int c, int r)
+// Define our function for printing characters in columns and rows
+void printfunc(char ch, int c, int r)
 {
+	// Private vars for our function to use
 	int col, row;
 
+	// Initialze row to 0, execute loop, and increment until while row is less than rows supplied by user
 	for (row = 0; row < r; row++)
 	{
+		// Initialize col to 0, execute loop, and increment while col is less than columns supplied by user
 		for (col = 0; col < c; col++)
+			// Use purchar to print character
 			putchar(ch);
 		putchar('\n');
 	}
 	
+	// Terminate function and return control to calling function
 	return;
 }
